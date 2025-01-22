9c2817a4817fbc87a525a7a4e6542cc90fac1479
yogiyu1002 <yu.xiaoj@northeastern.edu>
2025-01-22 15:09:24 -0800

    add full name

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h2>Xiaojun Yu- CS5610</h2>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
